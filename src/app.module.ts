e23d637e0171bb0a3dafade395df7a180e2b2ad6
ponchik009 <lolioknru@mail.ru>
2022-03-30 16:59:51 +0800

    postgres в докере, подключение swagger, подключение к базе

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+require('dotenv').config();
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
+  providers: [],
+  controllers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+require('dotenv').config();
+
+async function start() {
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Система учета заявок IT подразделения')
+    .setDescription('Проект академии IT')
+    .setVersion('0.0.1')
+    .addTag('ponchik009')
+    .build();
+
+  app.setGlobalPrefix('api');
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
+  await app.listen(PORT, () => console.log(`Server starts on port ${PORT}`));
 }
-bootstrap();
+
+start();
